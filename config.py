84acccf4e332584407595c63f35484227c976cd7
infinity1984-infi <ind.agamer7@gmail.com>
2025-04-12 21:20:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,9 @@ class Config:
     CHANNEL_ID = -1002610839118  # Replace with your channel ID (include -100)
     BASE_MESSAGE_ID = 4   # Starting message ID (e.g., 1 → 4, 2 → 5)
     ADMINS = [6285668838]  # Your Telegram user ID (get from @userinfobot)
-    FORCE_SUB_CHANNEL = -1001234567890  # Your channel ID (optional: set to None to disable)
+    FORCE_SUB_CHANNEL = -1002590122361  # Your channel ID (optional: set to None to disable)
+    BUTTON_URLS = {
+        "button1": {"text": "📢 Channel", "url": "https://t.me/Hanime_System"},
+        "button2": {"text": "🌟 Rate Us", "url": "https://t.me/+pf-4srRnotc0YWQ1"}
+    }
+    
