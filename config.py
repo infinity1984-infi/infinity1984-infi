980e52353bc5475ac676d1067a05bf403092b44d
infinity1984-infi <ind.agamer7@gmail.com>
2025-04-12 16:30:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
-    API_ID = 23810894       # Get from https://my.telegram.org/apps
-    API_HASH = "7f51292061e6a64df8589ae7756e5603"
-    BOT_TOKEN = "7145224784:AAFKzcYMESW8k1bgxlCxl9w_CU5w1tlVQFI"
+    API_ID = 1234567       # Get from https://my.telegram.org/apps
+    API_HASH = "your_api_hash"
+    BOT_TOKEN = "your_bot_token"
     CHANNEL_ID = -1002610839118  # Replace with your channel ID (include -100)
     BASE_MESSAGE_ID = 4   # Starting message ID (e.g., 1 → 4, 2 → 5)
     ADMINS = [6285668838]  # Your Telegram user ID (get from @userinfobot)
